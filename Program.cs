3cb05a7291654d7ac809bab2d29e398a35d3e078
aaudric <audricgir@gmail.com>
2023-04-08 13:39:26 +0200

    de nombreux changement, désormais le jeu fonctionne

--- Program.cs
+++ Program.cs
@@ -1,107 +1,116 @@
-﻿using System;
+using System;
 
-namespace Game_in_C
-{
-    pulic class Program : Personnage
-    {
-        public void Main(string[] args)
-        {
-            Menu();
+class Character {
+    public string Name { get; set; }
+    public int Health { get; set; }
+    public int Attack { get; set; }
+    public int Defense { get; set; }
+
+    public Character(string name, int health, int attack, int defense) {
+        Name = name;
+        Health = health;
+        Attack = attack;
+        Defense = defense;
+    }
+
+    public virtual void AttackTarget(Character target) {
+        int damage = Attack - target.Defense;
+        if (damage < 0) damage = 0;
+        target.Health -= damage;
+        Console.WriteLine(Name + " attacks " + target.Name + " for " + damage + " damage!");
+    }
+}
+
+class Warrior : Character {
+    public Warrior(string name) : base(name, 100, 10, 5) {
+    }
+
+    public override void AttackTarget(Character target) {
+        int damage = Attack + 5 - target.Defense;
+        if (damage < 0) damage = 0;
+        target.Health -= damage;
+        Console.WriteLine(Name + " swings his sword and deals " + damage + " damage to " + target.Name + "!");
+    }
+}
+
+class Sorcerer : Character {
+    public Sorcerer(string name) : base(name, 80, 15, 3) {
+    }
+
+    public override void AttackTarget(Character target) {
+        int damage = Attack + 3 - target.Defense;
+        if (damage < 0) damage = 0;
+        target.Health -= damage;
+        Console.WriteLine(Name + " casts a spell on " + target.Name + " and deals " + damage + " damage!");
+    }
+}
+
+class Archer : Character {
+    public Archer(string name) : base(name, 90, 12, 4) {
+    }
+
+    public override void AttackTarget(Character target) {
+        int damage = Attack + 4 - target.Defense;
+        if (damage < 0) damage = 0;
+        target.Health -= damage;
+        Console.WriteLine(Name + " shoots an arrow at " + target.Name + " and deals " + damage + " damage!");
+    }
+}
+
+class Monster : Character {
+    public Monster() : base("Monster", 60, 10, 8) {
+    }
+}
+
+class Game {
+    public void Start() {
+        Console.WriteLine("Welcome to the game!");
+        Console.WriteLine("Choose your character:");
+        Console.WriteLine("1. Warrior");
+        Console.WriteLine("2. Sorcerer");
+        Console.WriteLine("3. Archer");
+
+        Character player = null;
+        int choice = int.Parse(Console.ReadLine());
+
+        switch (choice) {
+            case 1:
+                player = new Warrior("Player");
+                break;
+            case 2:
+                player = new Sorcerer("Player");
+                break;
+            case 3:
+                player = new Archer("Player");
+                break;
+            default:
+                Console.WriteLine("Invalid choice, please try again.");
+                return;
         }
 
-        static void Jouer(Personnage monPerso)
-        {
-            Monstre monstre =  new Monstre("Gobelin");
-            bool victory = true;
-            bool next = false;
+        Monster monster = new Monster();
 
-            while(!monstre.estMort())
-            {
-                //tour monstre
-                Console.ForegroundColor = ConsoleColor.Red;
-                monstre.Attaquer(monPerso);
-                Console.WriteLine();
-                Console.ReadKey(true);
-                
-                if(monPerso.estMort())
-                {
-                    victory = false;
-                    break; 
-                }
-
-                //tour perso 
-                Console.ForegroundColor = ConsoleColor.Green;
-                monPerso.Attaquer(monstre);
-                Console.WriteLine();
-                Console.ReadKey(true);
-            }
-
-            if(victory)
-            {
-                monPerso.gagnerExperience(5);
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.WriteLine();
-                Console.WriteLine(monPerso.Caracteristiques);
-
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine();
-
-                while(!next)
-                {
-                    Console.WriteLine("Next room ? (Y/N)");
-                    string a = Console.ReadLine().ToUpper();
-                    if (a == "Y")
-                    {
-                        next = true;
-                        Jouer(monPerso);
-                    }
-                    else if (a == "N")
-                    {
-                        Environment.Exit(0);
-                    }
-                }
-            }
-            else{
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine();
-                Console.WriteLine("You lost ...");
-                Console.ReadKey();
-            }
-        }
-        static void Menu()
-        {
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("The Game");
+        while (player.Health > 0 && monster.Health > 0) {
+            Console.WriteLine(player.Name + ": " + player.Health + " HP");
+            Console.WriteLine(monster.Name + ": " + monster.Health + " HP");
             Console.WriteLine();
-            Console.WriteLine("Choose your class : ");
-            Console.WriteLine("1. Warrior");
-            Console.WriteLine("2. Wizard");
-            Console.WriteLine("3. Archer");
-            Console.WriteLine("4. Exit");
-            Console.WriteLine();
 
-            switch (Console.ReadLine())
-            {
-                case "1": 
-                    Console.WriteLine("You chose a Warrior");
-                    Console.WriteLine();
-                    Jouer(new Guerrier("Audric"));
-                    break;
-                case "2": 
-                    Console.WriteLine("You chose a Wizard");
-                    Console.WriteLine();
-                    Jouer(new Guerrier("Audric"));
-                    break;
-                case "3": 
-                    Console.WriteLine("You chose an Archer");
-                    Console.WriteLine();
-                    Jouer(new Guerrier("Audric"));
-                    break;
-
-                case "4": 
-                    break;
+            player.AttackTarget(monster);
+            if (monster.Health > 0) {
+                monster.AttackTarget(player);
             }
-
         }
+
+        if (player.Health > 0)
+        {
+            Console.WriteLine("You win!");
+        } else {
+            Console.WriteLine("You lose...");
+        }
+    }}
+    class Program {
+    static void Main(string[] args) {
+        Game game = new Game();
+        game.Start();
     }
 }
